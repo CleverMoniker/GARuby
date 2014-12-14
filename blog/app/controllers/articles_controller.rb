11d6269457546de36470972f319cf6ea488c78fd
CleverMoniker <david.l.langlois@gmail.com>
2014-12-14 17:09:14 -0600

    Added lots...

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+
+	def new
+
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def create
+		# render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		@article.save
+
+		redirect_to @article
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'eidt'
+		end
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+end
